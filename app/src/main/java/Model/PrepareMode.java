2a0c10558c79e75657a10e717004cdb0b893b363
lauralassance <laura.lassance@gmail.com>
2015-05-16 23:16:52 -0300

    Adapting to parse objects

--- app/src/main/java/Model/PrepareMode.java
+++ app/src/main/java/Model/PrepareMode.java
@@ -1,13 +1,41 @@
 package Model;
 
+import com.parse.ParseObject;
+import com.parse.ParseClassName;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by User on 5/16/2015.
  */
-public class PrepareMode {
+@ParseClassName("PrepareMode")
+public class PrepareMode extends ParseObject {
     private ArrayList<PreparationStep> steps;
     private int currentStep;
+
+    public ArrayList<PreparationStep> getSteps() {
+        return (ArrayList) getList("steps");
+    }
+
+    public void setSteps(ArrayList<PreparationStep> steps) {
+        put("steps", steps);
+    }
+
+    public int getCurrentStep() {
+        return currentStep;
+    }
+
+    public void setCurrentStep(int currentStep) {
+        this.currentStep = currentStep;
+    }
+
+    public PrepareMode() {
+        this(null);
+    }
 
     public PrepareMode(ArrayList<PreparationStep> steps) {
         this.steps = steps;
@@ -29,5 +57,7 @@ public class PrepareMode {
     public int howManySteps() {
         return steps.size();
     }
+
+
 
 }
